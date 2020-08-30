006ada60c4642a30ac98213eac5aa3311179ff3c
Rimesh <rimeshlwagun10@gmail.com>
2020-08-30 14:27:09 +0545

    add country search

--- classes/corona_data.py
+++ classes/corona_data.py
@@ -10,36 +10,48 @@ Coded by: Abiskar Timsina
 import requests as req
 from bs4 import *
 import sys
+import pprint
 
 class corona_data_scrapper:
 
-    def data_scrapper(self,up_url):
+    def data_scrapper(self,up_url, country_list = None):
         url = up_url
         temp_list = []
         soup = BeautifulSoup(url, 'lxml')
         body = soup.find('div', class_="table-scroll")
         table_row = body.table.tbody.find_all('tr')
 
+        table_head = body.table.find_all('th')
+        header = [a.text.replace('\n',' ').strip(' ') for a in table_head]
+
+        print(header)
+        print(type(country_list))
+        
+        if country_list != None :
+            for country in country_list:
+                for num, row in enumerate(table_row):
+                    if country.lower() in row.find_all('td')[1].text.replace('\n',' ').strip(' ').lower():
+                        temp_list = [a.text.replace('\n',' ').strip(' ') for a in row.find_all('td')]
+                        temp_list = dict(zip(header, temp_list))
+                        pprint.pprint(temp_list)
+                        break
+            sys.exit()
+
         for exit_control, data in enumerate(table_row):
-            table_row_data = data.find_all('td')
-            for ind_data_row in table_row_data:
-                temp_format1 = ind_data_row.text.replace('\n', ' ')
-                temp_list.append(temp_format1)
-            print(temp_list)
-            print()
-            temp_list = []
+            temp_list = [a.text.replace('\n',' ').strip(' ') for a in data.find_all('td')]
+            temp_list = dict(zip(header, temp_list))
+            pprint.pprint(temp_list)
             if (exit_control == 4):
                 break
 
-    def main(self):
+    def main(self, country_list = None):
         try:
             unparsed_url = req.get("https://virusncov.com/").text
-            self.data_scrapper(unparsed_url)
-        except:
+            self.data_scrapper(unparsed_url, country_list)
+        except Exception as err:
             print('The bot cannot retrieve the requested info. Check your connection and try again later.')
+            print(f'the error occurred : \n {err}')
             sys.exit()
-
-
 
 
 class corona_news_scrapper:
--- main.py
+++ main.py
@@ -6,16 +6,27 @@ Coded by: Abiskar Timsina
 
 """
 from classes.corona_data import *
-
+import sys
 
 obj_corona_data = corona_data_scrapper()
 obj_corona_news = corona_news_scrapper()
 
-while (1):
-    a = input ("Welcome to Webscrepper: ")
-    if (a == "!news"):
-        obj_corona_news.main()
-    elif (a == "!update"):
-        obj_corona_data.main()
-    else:
-        continue
+if '-h' in sys.argv:
+	print("# usage of the script ")
+	print("$ python main.py 			\n-- takes you to main program for news and update features.")
+	print("$ python main.py country <county_name>\n -- print the data of the country entered.")
+	print("also\n$ python main.py county <county_name1> <county_name2> <county_name3> ...")
+	print("-- prints the data of multiple countries.")
+
+if len(sys.argv) == 1: 
+	while (1):
+		a = input ("Welcome to Webscrepper: ")
+		if (a == "!news"):
+			obj_corona_news.main()
+		elif (a == "!update"):
+			obj_corona_data.main()
+		else:
+			continue
+elif len(sys.argv) > 1:
+	if 'country' in sys.argv[1].lower():
+		obj_corona_data.main(sys.argv[2:]) 
